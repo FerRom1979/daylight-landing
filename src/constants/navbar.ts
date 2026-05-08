84692d8c8bca5a2d7a1c60910c40a31bafa80e85
Fernando Romero <fernando_0135@hotmail.com>
2026-05-08 10:43:38 -0300

    feat: add prettier

---
+++ src/constants/navbar.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { label: 'navbar.home', href: '#' },
+  { label: 'navbar.devocionals', href: '#' },
+  { label: 'navbar.community', href: '#' },
+  { label: 'navbar.about', href: '#' },
+];
